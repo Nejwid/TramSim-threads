e90c85a0efd5ef395f90a5684693a85e6cb3ac80
Dawid <273202@student.pwr.edu.pl>
2026-01-12 23:02:33 +0100

    added lint workflow runner

--- finalSrc/tests/test_main.cpp
+++ finalSrc/tests/test_main.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../src/trafficManager/TrafficManager.h"
+#include "../src/depots/Depot.h"
 
 // clean build -> build again to see tests changes
 
@@ -9,4 +10,11 @@ TEST_CASE("singleton check") {
     TrafficManager* test2 = TrafficManager::GetInstance();
     REQUIRE(test2 != nullptr);
     REQUIRE(test1 == test2);
+}
+
+TEST_CASE("tram depot (factory) check") {
+    unique_ptr<Depot> testDepot = make_unique<GajDepot>();
+    unique_ptr<Tram> test1 = testDepot->TramNumberOne(1);
+    unique_ptr<Tram> test2 = testDepot->TramNumberOne(1);
+    REQUIRE(test1 != test2);
 }
